60e2a5a7927537367ef96733bbcb31131d043fab
Dale Schumacher <dalnefre@yahoo.com>
2021-01-07 18:22:21 -0700

    use mA for milliamps

--- tutorial/counter/count_3_fomu.v
+++ tutorial/counter/count_3_fomu.v
@@ -36,21 +36,21 @@ module fomu_pvt (
   localparam RGBA_CURRENT_MODE_FULL = "0b0";
   localparam RGBA_CURRENT_MODE_HALF = "0b1";
   // Current levels in Full / Half mode
-  localparam RGBA_CURRENT_04MA_02MA = "0b000001";
-  localparam RGBA_CURRENT_08MA_04MA = "0b000011";
-  localparam RGBA_CURRENT_12MA_06MA = "0b000111";
-  localparam RGBA_CURRENT_16MA_08MA = "0b001111";
-  localparam RGBA_CURRENT_20MA_10MA = "0b011111";
-  localparam RGBA_CURRENT_24MA_12MA = "0b111111";
+  localparam RGBA_CURRENT_04mA_02mA = "0b000001";
+  localparam RGBA_CURRENT_08mA_04mA = "0b000011";
+  localparam RGBA_CURRENT_12mA_06mA = "0b000111";
+  localparam RGBA_CURRENT_16mA_08mA = "0b001111";
+  localparam RGBA_CURRENT_20mA_10mA = "0b011111";
+  localparam RGBA_CURRENT_24mA_12mA = "0b111111";
 
   // Instantiate iCE40 LED driver hard logic
   wire LED_r, LED_g, LED_b;
   SB_RGBA_DRV #(
     .CURRENT_MODE(RGBA_CURRENT_MODE_HALF),
-//    .RGB0_CURRENT(RGBA_CURRENT_08MA_04MA),
-    .RGB0_CURRENT(RGBA_CURRENT_16MA_08MA),  // green needs more current
-    .RGB1_CURRENT(RGBA_CURRENT_08MA_04MA),
-    .RGB2_CURRENT(RGBA_CURRENT_08MA_04MA)
+//    .RGB0_CURRENT(RGBA_CURRENT_08mA_04mA),
+    .RGB0_CURRENT(RGBA_CURRENT_16mA_08mA),  // green needs more current
+    .RGB1_CURRENT(RGBA_CURRENT_08mA_04mA),
+    .RGB2_CURRENT(RGBA_CURRENT_08mA_04mA)
   ) RGBA_DRIVER (
     .CURREN(1'b1),
     .RGBLEDEN(1'b1),
